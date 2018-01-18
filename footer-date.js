a64c2b33ea3a9b98b077ebbce10f9eefa9137f9f
MaX Falstein <MaXFalstein@users.noreply.github.com>
2018-01-18 12:33:13 +0000

    Create footer-date.js

---
+++ footer-date.js
@@ -0,0 +1,11 @@
+var d = new Date();
+var y = 0;
+var year = d.getFullYear().toString();
+var m = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+var month = m[d.getMonth()];
+var footer = "";
+var footer0 = "MaX Falstein gTLD &copy; MaX Falstein and Falstein Inc. ";
+var footer1 = " ";
+var footer2 = " &#8212; Patents and Trademarks Pending";
+footer = footer0.concat(month.concat(footer1.concat(year.concat(footer2))));
+document.getElementById("footer").innerHTML = footer;
